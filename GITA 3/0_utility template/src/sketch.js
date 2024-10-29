2aaceb29b5d802e60882d79c3796f51eb47b54e6
Susan Yoon <awesomeyooner@gmail.com>
2024-10-29 12:17:12 -0700

    added template

---
+++ GITA 3/0_utility template/src/sketch.js
@@ -0,0 +1,30 @@
+
+var mouse = new Mouse();
+var keyboard = new KeyboardController();
+
+
+function setup() {
+  createCanvas(1280, 720);
+  frameRate(60);
+
+  keyboard.initialize(window);
+  keyboard.configureBinding("c", () => print("hello"));
+
+  mouse.configureBinding(() => print("PRESS"), MouseState.ON_PRESS);
+  mouse.configureBinding(() => print("RELEASE"), MouseState.ON_RELEASE);
+  mouse.configureBinding(() => print("NEUTRAL"), MouseState.WHILE_UP);
+  mouse.configureBinding(() => print("HELD"), MouseState.WHILE_DOWN);
+}
+
+function draw(){
+  background(220);
+
+  mouse.update(mouseIsPressed);
+
+}
+
+function logData(){
+  for(var key of keyboard.getKeys().keys()){
+    print(key + ": " + keyboard.getKeys().get(key));
+  }
+}
