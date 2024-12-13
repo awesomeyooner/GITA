1e15a9ed0da77979989298924b1836f1fae98662
Susan Yoon <awesomeyooner@gmail.com>
2024-12-12 18:46:11 -0800

    dsadsad

---
+++ Portfolio/main/assets/projects/gita1.js
@@ -0,0 +1,41 @@
+const pathToProjects = "assets/projects/GITA3/";
+
+const jsonData = 
+[
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    }
+]
---
+++ Portfolio/main/assets/projects/gita2.js
@@ -0,0 +1,39 @@
+const jsonData = 
+[
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    },
+    {
+        "header": "Project",
+        "image": "assets/projects/GITA3/Avatar/avatar.png",
+        "link": "https://classroom.google.com/u/1/c/NjkxNzExOTA3MDA1",
+        "description": "This is my avatar!"
+    }
+]
