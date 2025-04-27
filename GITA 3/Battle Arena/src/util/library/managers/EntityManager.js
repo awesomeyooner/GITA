d49bc4a9cd40f841f31207aef809a1b693fd8094
Susan Yoon <awesomeyooner@gmail.com>
2025-04-26 20:46:21 -0700

    added entity manager

---
+++ GITA 3/Battle Arena/src/util/library/managers/EntityManager.js
@@ -0,0 +1,59 @@
+class EntityManager{
+
+    constructor(maxEntities = 100){
+        this.maxEntities = maxEntities;
+
+        this.entities = new Array();
+    }
+
+    /**
+     * Call in the constructor
+     */
+    initialize(){
+        // this.reset(50, 1);
+    }
+
+    update(){
+        for(var entity of this.entities){
+            entity.update();
+        }
+    }
+
+    /**
+     * Override, basically just populate the array
+     */
+    reset(){}
+
+    respawnOne(origin){
+
+        for(var entity of this.entities){
+            if(entity.isActive)
+                continue;
+
+            entity.reset(origin);
+            break;
+        }
+    }
+
+    getNumberOfActiveEntities(){
+        var total = 0;
+
+        for(var entity of this.entities){
+            if(entity.isActive)
+                total++;
+        }
+
+        return total;
+    }
+
+    getNumberOfInactiveEntities(){
+        var total = 0;
+
+        for(var entity of this.entities){
+            if(!entity.isActive)
+                total++;
+        }
+
+        return total;
+    }
+}
--- GITA 3/Battle Arena/src/util/units/Turret.js
+++ GITA 3/Battle Arena/src/util/units/Turret.js
@@ -1,3 +1,43 @@
 class Turret extends Gunner{
-    
+ 
+    constructor(size, maxBullets, maxHealth, color="green"){
+        super(
+            size,
+            0,
+            maxBullets,
+            maxHealth,
+            color
+        );
+
+        this.initialize();
+    }
+
+    initialize(){
+
+    }
+
+    update(enemies){
+        super.update();
+
+        if(!this.isActive)
+            return;
+
+        this.shootAtClosest(enemies);
+    }
+
+    shootAtClosest(enemies){
+
+        var proc = 20; //20 in 1000
+
+        var random = Math.round(Utility.random(0, 1000));
+
+        if(proc != random)
+            return
+
+        var closest = this.getClosestTarget(enemies);
+
+        this.shoot(closest);
+    }
+
+
 }
